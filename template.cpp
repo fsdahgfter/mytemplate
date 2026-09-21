1d72b490f7db84170e01966d1ad39bd6de3a0756
fsdahgfter <cop10hcm@gmail.com>
2026-09-21 17:25:58 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for(int i = (a); i <= (b); ++i)
+#define FORD(i, b, a) for(int i = (b); i >= (a); --i)
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FILE(NAME) if(fopen(NAME".INP", "r")) { freopen(NAME".INP", "r", stdin); freopen(NAME".OUT", "w", stdout); }
+
+template<class X, class Y> bool maximize(X &x, const Y &y) { return x < y ? x = y, 1 : 0; }
+template<class X, class Y> bool minimize(X &x, const Y &y) { return x > y ? x = y, 1 : 0; }
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    FILE("NAME")
+    
+    return 0;
+}
