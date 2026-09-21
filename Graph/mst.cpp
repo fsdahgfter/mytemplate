7d508d31fc13fcc02722d137ca161b326c6abb18
fsdahgfter <cop10hcm@gmail.com>
2026-09-21 19:28:01 +0700

    Update mst.cpp

--- Graph/mst.cpp
+++ Graph/mst.cpp
@@ -1,1 +1,18 @@
+struct edge {
+    int u, v, w;
+    bool operator < (const edge &a) const {
+        return w < a.w;
+    }
+};
 
+ll getMST(vector<edge> &edges) {
+    DSU dsu;
+    sort(all(edges));
+    
+    ll mst = 0;
+    for(edge &i : edges) if(dsu.unite(i.u, i.v)) {
+        mst += i.w;
+    }
+
+    return mst;
+}
