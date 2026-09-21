f648b7e761ef6167b06447267ab8b906e707aa6d
fsdahgfter <cop10hcm@gmail.com>
2026-09-21 19:31:34 +0700

    Update bfs.cpp

--- Graph/Shortest Path/bfs.cpp
+++ Graph/Shortest Path/bfs.cpp
@@ -1,1 +1,16 @@
+vector<int> adj[N];
+int dist[N];
 
+void bfs(int s) {
+    memset(dist, -1, sizeof dist);
+    queue<int> q; q.emplace(s);
+    dist[s] = 0;
+
+    while(!q.empty()) {
+        int u = q.front(); q.pop();
+        for(int v : adj[u]) if(dist[v] == -1) {
+            dist[v] = dist[u] + 1;
+            q.emplace_back(v);
+        }
+    }
+}
