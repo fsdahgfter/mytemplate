e2b97700d88d6ffebea254a8de117f7092e28d8d
fsdahgfter <cop10hcm@gmail.com>
2026-09-22 12:19:44 +0700

    Update Eratosthenes.cpp

--- Number Theory/Eratosthenes.cpp
+++ Number Theory/Eratosthenes.cpp
@@ -1,1 +1,13 @@
+const int maxA = ;
 
+bool not_prime[maxA + 5];
+// vector<int> primes;
+
+void sieve() {
+    not_prime[0] = not_prime[1] = 1;
+    for(int i = 2; i * i <= maxA; ++i) if(!not_prime[i]) {
+        for(int j = i * i; j <= maxA; j += i) not_prime[j] = 1;
+    }
+
+    // for(int i = 2; i * i <= maxA; ++i) if(!not_prime[i]) primes.emplace_back(i);
+}
